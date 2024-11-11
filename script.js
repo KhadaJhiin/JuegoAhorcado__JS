6815dc109072e6ab2ba13e280693666567b706e9
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2024-11-11 16:20:29 -0500

    Se termina la parte fuerte de la logica del juego

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //ELEMENTOS DEL DOM
 let cajaDeIntentos = document.getElementById('caja__de--intentos');
 let formulario = document.querySelector(".entrada__palabra");
+let inputDeEntrada = document.getElementById("unicoEntradaUsuario");
 
 //Variables declaradas
-const listaDePalabras = ['zapato', 'perro', 'mañana','cocodrilo','por'];
+const listaDePalabras = ['zapato', 'perro', 'mañana', 'cocodrilo', 'aguardiente','vinito','cocina','libros','cientifico','automovil','tridente','espabilado','ipsofacto','infraganti','somnoliento'];
 let numeroDeIntentos = 5;
+const palabraSecreta = palabraAleatoria();
+const arrayPalabra = palabraSecreta.split('');
 
 //EVENTOSLISTENER
 document.getElementById("probarLetra").addEventListener('click', probarLetra);
@@ -26,18 +29,52 @@ function palabraAleatoria() {
 function mostrarNumeroIntentos() {
     cajaDeIntentos.innerText = ' ' + numeroDeIntentos;
 }
+function mostrarEspaciosParaLetra() {
+    formulario.innerHTML = "";
+    arrayPalabra.forEach((element, index) => {
+        formulario.innerHTML += `<input disabled = false class="inputName" type="text" name="" id="letraPalabra${index}" value="">`;
+    });
+}
+function buscarIndiceLetras(letra, palabra) {
+    let listaIndices = [];
+    palabra.forEach((element, index) => {
+        if (letra == element) {
+            listaIndices.push(index);
+        }
+    })
+    return listaIndices;
+}
+function tranformadorInputString() {
+    let palabra = '';
+    for (let i = 0; i < arrayPalabra.length; i++) {
+        let letraEncontrada = document.getElementById(`letraPalabra${i}`);
+        palabra += letraEncontrada.value;
+    }
+    return palabra;
+}
 function probarLetra() {
+    let letra = inputDeEntrada.value;
     if (numeroDeIntentos > 0) {
-        numeroDeIntentos--;
-        mostrarNumeroIntentos();
+        if (arrayPalabra.includes(letra)) {
+            let listaIndices = buscarIndiceLetras(letra, arrayPalabra);
+            listaIndices.forEach(element => {
+                let letraEncontrada = document.getElementById(`letraPalabra${element}`);
+                letraEncontrada.value = letra;
+            })
+        } else {
+            numeroDeIntentos--;
+            mostrarNumeroIntentos()
+        }
     } else {
         alert('perdiste das lastima mi amor!');
         location.reload();
     }
+    inputDeEntrada.value = "";
+    
+    let palabraUsuario = tranformadorInputString();
+    if (palabraUsuario == palabraSecreta) {
+        alert('GANASTE MI PERRO LO SALVASTE AL ASESINO')
+        location.reload();
+    }
 }
-function mostrarEspaciosParaLetra() {
-    formulario.innerHTML="";
-    palabraAleatoria().split('').forEach((element,index) => {
-        formulario.innerHTML += `<input type="text" name="" id="letraPalabra${index}">`;
-    });
-}
+
